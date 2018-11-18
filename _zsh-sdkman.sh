2e573f9184a2c79b1c37290224195d52bc41d7c6
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2018-11-18 23:01:48 +0100

    feat: developing install and uninstall completions
    Also added some sdkman aliases

--- _zsh-sdkman.sh
+++ _zsh-sdkman.sh
@@ -1,15 +1,22 @@
 #compdef sdk
 
-# TODO Should this be activated or not ?
-# zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*:descriptions' format '%B%d%b'
 # zstyle ':completion::complete:sdk:*:commands' group-name commands
+# zstyle ':completion::all_candidates:sdk:*:all_candidates' group-name all_candidates
+# zstyle ':completion::candidates_to_uninstall:sdk:*:candidates_to_uninstall' group-name candidates_to_uninstall
 # zstyle ':completion::complete:sdk::' list-grouped
 
-# zmodload zsh/mapfile
+# Gets candidate lists and removes all unecessery things just to get candidate names
+__get_candidate_list() {
+  echo `sdk list | grep --color=never  "$ sdk install" | sed 's/\$ sdk install //g' | sed -e 's/[\t ]//g;/^$/d'`
+}
 
-# TODO Add a plugin file to add aliases
+# Gets installed candidates list
+__get_current_installed_list() {
+  echo `sdk current | sed "s/Using://g" | sed "s/\:.*//g"  | sed -e "s/[\t ]//g;/^$/d"`
+}
 
-_describe_commands() {
+__describe_commands() {
   local -a commands
   commands=(
     'install: install a program'
@@ -30,11 +37,43 @@ _describe_commands() {
   _describe -t commands "Commands" commands && ret=0
 }
 
-_describe_offline() {
+__describe_install() {
+  local -a candidate_list
+  candidate_list=( `sdk list | grep --color=never  "$ sdk install" | sed 's/\$ sdk install //g' | sed -e 's/[\t ]//g;/^$/d'` )
+  _describe -t candidate_list "Candidates available" candidate_list && ret=0
+}
+
+__describe_uninstall() { # FIXME THis is not working, it only displays candidate list rather than installed list
+  local -a candidates_to_uninstall
+  candidates_to_uninstall=( `sdk current | sed "s/Using://g" | sed "s/\:.*//g"  | sed -e "s/[\t ]//g;/^$/d"` )
+  _describe -t candidates_to_uninstall "Uninstallable candidates" candidates_to_uninstall && ret=0
+}
+
+__describe_list() {
+  # TODO
+}
+
+__describe_use() {
+  # TODO
+}
+
+__describe_default() {
+  # TODO
+}
+
+__describe_current() {
+  # TODO
+}
+
+__describe_upgrade() { # FIXME THis is not working, it only displays candidate list
+  # TODO
+}
+
+__describe_offline() {
   local -a offline
 
   offline=(
-    'enabled: sdkman wo'
+    'enabled'
     'disabled'
   )
 
@@ -45,20 +84,44 @@ function _sdk() {
   local ret=1
 
   local target=$words[2]
+  local candidate=$word[3]
 
-  # TODO There should be more arguments here
   _arguments -C \
     '1: :->first_arg' \
-    '2: :->second_arg' && ret=0
+    '2: :->second_arg' \
+    && ret=0
+    #'3: :->third_arg' && ret=0
 
+    # TODO Add a third_arg handling
     case $state in
       first_arg)
-        _describe_commands
+        __describe_commands
         ;;
       second_arg)
         case $target in
+          install)
+            __describe_install
+            ;;
+          uninstall)
+            __describe_uninstall
+            ;;
+          list)
+            __describe_list
+            ;;
+          use)
+            __describe_use
+            ;;
+          default)
+            __describe_default
+            ;;
+          current)
+            __describe_current
+            ;;
+          upgrade)
+            __describe_upgrade
+            ;;
           offline)
-            _describe_offline
+            __describe_offline
             ;;
           *)
             ;;
--- zsh-sdkman.plugin.zsh
+++ zsh-sdkman.plugin.zsh
@@ -1,3 +1,13 @@
 # aliases
 alias sdki='sdk install'
+alias sdkun='sdk uninstall'
+alias list='sdk list'
 alias sdku='sdk use'
+alias sdkd='sdk default'
+alias sdkc='sdk current'
+alias sdkup='sdk upgrade'
+alias sdkv='sdk version'
+alias sdkb='sdk broadcast'
+alias sdko='sdk offline'
+alias sdksu='sdk selfupdate'
+alias sdkf='sdk flush'
