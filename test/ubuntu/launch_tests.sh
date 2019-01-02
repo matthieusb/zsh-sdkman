ccd320e6d3d001440e7bccc8b70ad5b68b1f8e9d
Matthieu SAUBOUA-BENELUZ <msauboua@ippon.fr>
2019-01-02 10:20:44 +0100

    test: corrected an error to force relaunch the file generation script

--- test/ubuntu/launch_tests.sh
+++ test/ubuntu/launch_tests.sh
@@ -3,8 +3,7 @@
 echo '### Launching tests scripts ###'
 
 echo '### Generating candidates files through zsh-sdkman plugin ###'
-# Not needed, the files are already being generated when launching the container
-# /usr/bin/zsh -c "source ~/.zshrc && sdk-refresh-completion-files"
+/usr/bin/zsh -c "source ~/.zshrc && sdk-refresh-completion-files"
 
 echo '### Launching shunit2 tests ###'
 export SHUNIT_PARENT="candidates_tests.sh"
